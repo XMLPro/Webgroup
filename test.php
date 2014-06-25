6536daf155cbeb59ab04492d2ef89f1f5edd17b9
Zen Obara <rioner2525@gmail.com>
2014-06-25 09:11:58 +0900

    issue

--- test.php
+++ test.php
@@ -11,36 +11,27 @@ $user = 'WebGroup';
 $password = 'divtyu';
 try{
     $dbh = new PDO($dsn, $user, $password);
-	if ($dbh == null){
-        header('Location: issue.html');
-		exit();
-    }
 	$dbh->query('SET NAMES UTF-8');
 	$name = $_POST['username'];
 	$sql = 'select * from user';
-    foreach ($dbh->query($sql) as $row) {    
-        if($name == $row['password']){
-			header('Location: issue.html');
-			exit();
+    foreach ($dbh->query($sql) as $row) {
+		if($name == $row['password']){
+        	header('Location: issue.html');
+			exit;
 		}
     }
-	$sql = 'INSERT INTO user (password) VALUES (?)';
-	$stmt = $dbh->prepare($sql);
-	$flag = $stmt->execute(array($name));
-	if ($flag){
-        header('Location: index.html');
-		exit();
-    }else{
-        header('Location: issue.html');
-		exit();
-    }
+	header('Location: index.html');
+	exit;
 }catch (PDOException $e){
     print('Error:'.$e->getMessage());
     die();
 }
 
 $dbh = null;
-
 ?>
+<form action="" method="post">
+	<input type="text" name="username">
+	<input type="submit" value="送信">
+</form>
 </body>
 </html>
